d6f275b5c3eb6a6411310ac12c8ad050c3328663
Joy\Dahee Joy Cha <ekgmlekgml20@naver.com>
2017-10-25 00:28:30 +0900

    first

---
+++ Assets/02. Scripts/FollowCam.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour {
+
+    public Transform targetTr; //추적할 타깃 게임 오브젝트의 Transform 변수
+    public float dist = 5f; //카메라와의 일정 거리
+    public float height = 4.0f; //카메라의 높이 설정
+    public float damTrace = 60.0f; //부드러운 추적을 위한 변수
+
+    //카메라 자신의 Transform 변수
+    private Transform tr;
+
+	// Use this for initialization
+	void Start () {
+        //카메라 자신의 Transfrom 컴포넌트를 tr에 할당
+        tr = GetComponent<Transform>();
+	}
+	
+	//타깃 이동 종료 후 카메라 추적하기 위해 LateUpdate
+    void LateUpdate()
+    {
+        tr.position = Vector3.Lerp(tr.position, targetTr.position - (targetTr.forward * dist) + (Vector3.up * height), Time.deltaTime * damTrace);
+        //카메라가 타깃 게임 오브젝트 바라보게 설정
+        tr.LookAt(targetTr.position);
+    }
+}
---
+++ Assets/02. Scripts/PlayerCtrl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+	private float h = 0.0f;
+	private float v = 0.0f;
+
+	//접근 필요한 컴포넌트는 변수에 할당
+	private Transform tr;
+
+	//이동 속도 변수
+	public float moveSpeed = 10.0f;
+
+    //회전 속도 변수 
+    public float rotSpeed = 100.0f;
+	// Use this for initialization
+	void Start () {
+		tr = GetComponent<Transform> ();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		h = Input.GetAxisRaw("Horizontal");
+		v = Input.GetAxisRaw ("Vertical");
+
+		Debug.Log("H=" + h.ToString());
+		Debug.Log("V=" + v.ToString());
+
+        //전후좌우 이동방향 벡터 계산
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        //Translate(이동방향*속도*변위값*Time.deltaTime, 기준좌표)
+        tr.Translate(moveDir.normalized * moveSpeed  * Time.deltaTime, Space.Self);
+
+        //Vector3.up축을 기준으로 rotSpeed만큼의 속도로 회전
+        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+
+	}
+}
