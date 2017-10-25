43283fba55a4ef054ff777531196daece2669b8d
Joy\Dahee Joy Cha <ekgmlekgml20@naver.com>
2017-10-25 19:51:47 +0900

    총알 발사 함수 완료

---
+++ Assets/02. Scripts/FireCtrl.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCtrl : MonoBehaviour
+{
+    //총알 프리팹
+    public GameObject bullet;
+    //총알 발사 좌표
+    public Transform firePos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //마우스 왼쪽 버튼 클릭시 Fire 함수 호출
+        if (Input.GetMouseButtonDown(0))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        //동적으로 총알 생성해주는 함수
+        CreateBullet();
+    }
+
+    void CreateBullet()
+    {
+        //Bullet 프리팹을 동적으로 생성
+        Instantiate(bullet, firePos.position, firePos.rotation);
+    }
+}
---
+++ Assets/02. Scripts/WallCtrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCtrl : MonoBehaviour {
+
+	//충돌 시작에서 발생하는 이벤트
+    void OnCollisionEnter(Collision coll)
+    {
+        //충돌 게임 오브젝트의 태그 값 비교
+        if(coll.collider.tag == "BULLET")
+        {
+            //충돌 게임 오브젝트 삭제
+            Destroy(coll.gameObject);
+        }
+    }
+}
